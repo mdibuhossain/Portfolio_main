b8807ad2ecb175222ad259d2c0cb0e2767118ca1
Ibrahim H <mohammad.ihakash@gmail.com>
2021-12-08 02:17:46 +0600

    portfolio project hover

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,6 +7,7 @@ const Product = (props) => {
     const { name, images } = props.product;
     const useStyle = makeStyles((theme) => createStyles({
         showProduct: {
+            position: 'relative',
             width: '100%',
             height: '350px',
             borderRadius: '12px',
@@ -14,19 +15,30 @@ const Product = (props) => {
             // objectFit: 'cover',
             overflow: 'hidden'
         },
+        projectImg: {
+            '&:hover': {
+                transform: 'translate(0%, calc(-100% + 350px))'
+            },
+            position: 'absolute',
+            zIndex: '10',
+            width: '100%',
+            transition: '1s ease-in-out'
+        },
         imgContainer: {
             padding: '8px',
             borderRadius: '15px',
-            background: 'rgba(230, 237, 244,0.6)',
+            background: 'rgba(255, 255, 255,0.15)',
+            boxShadow: 'inset 0 0 0 200px rgba(255,255,255,0.08)',
+            backdropFilter: 'blur(5px)'
         }
     }));
-    const { showProduct, imgContainer } = useStyle();
+    const { showProduct, imgContainer, projectImg } = useStyle();
     return (
         <>
             <Grid item xs={12} md={4}>
                 <Box className={imgContainer}>
                     <Box className={showProduct}>
-                        <img style={{ width: "100%" }} src={images[0]} alt="" />
+                        <img className={projectImg} src={images[0]} alt="" />
                     </Box>
                 </Box>
             </Grid>
