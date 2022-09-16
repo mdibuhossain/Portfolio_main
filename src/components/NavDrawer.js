615e29b18f975c0596900fe59d0efdafaaf9ca91
Ibrahim H <mohammad.ihakash@gmail.com>
2022-09-16 19:01:50 +0600

    Updated

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -159,6 +159,9 @@ export default function NavDrawer() {
                     open={mobileOpen}
                     onClose={handleDrawerToggle}
                     onOpen={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: false,
+                    }}
                     PaperProps={{
                         sx: {
                             position: 'relative',
